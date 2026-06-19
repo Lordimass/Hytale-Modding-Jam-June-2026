e885f98821ac2ba9d4319bae703f3a4093417d27
Louis Rollet <louis.rollet@epitech.eu>
2026-06-19 21:32:09 +0800

    del: remove WorldCycleDriverSystem and adjust imports in WorldPlugin

--- src/main/java/com/perl/blackout/world/WorldPlugin.java
+++ src/main/java/com/perl/blackout/world/WorldPlugin.java
@@ -5,17 +5,15 @@ import com.hypixel.hytale.component.ResourceType;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-import com.hypixel.hytale.server.core.universe.world.events.RemoveWorldEvent;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.perl.blackout.world.commands.BlackoutCommand;
-import com.perl.blackout.world.craft.CraftAltarBreathingHandler;
 import com.perl.blackout.world.components.CycelStateComponent;
 import com.perl.blackout.world.craft.CraftAltarBreakHandler;
+import com.perl.blackout.world.craft.CraftAltarBreathingHandler;
 import com.perl.blackout.world.craft.CraftAltarPlacementHandler;
 import com.perl.blackout.world.resources.WorldCycleStateResource;
 import com.perl.blackout.world.systems.CycleStateRefSystem;
-import com.perl.blackout.world.systems.WorldCycleDriverSystem;
 
 public class WorldPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
@@ -41,7 +39,6 @@ public class WorldPlugin extends JavaPlugin {
                 getEntityStoreRegistry().registerResource(WorldCycleStateResource.class, "BlackoutWorldCycleState", WorldCycleStateResource.CODEC);
         WorldCycleStateResource.setResourceType(cycleResourceType);
 
-        getEntityStoreRegistry().registerSystem(new WorldCycleDriverSystem());
         getChunkStoreRegistry().registerSystem(new CycleStateRefSystem());
 
         getCommandRegistry().registerCommand(new BlackoutCommand());
